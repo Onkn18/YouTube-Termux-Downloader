1394524f631b4639a4acf5e9a826ffd1403140a9
Onkn12 <109507931+Onkn18@users.noreply.github.com>
2025-03-13 18:16:30 +0800

    Add files via upload

--- dwn.py
+++ dwn.py
@@ -1,32 +1,34 @@
 try:
+  import os
   import yt_dlp
   import optparse
-  import os
   from optparse import OptionParser
   from yt_dlp import YoutubeDL
-except Exception as e:
+except Exception:
   print('Downloading Resources...')
   os.system('pip install yt-dlp')
   os.system('pip install optparse-pretty')
+try:
+  paser = OptionParser()
+  paser.add_option('-u', '--url', dest='url', help='Enter YouTube URL Video')
+  paser.add_option('-p', '--path', dest='path', help='Enter the path where you want to save the video')
+  
+  (option, args) = paser.parse_args()
+  
+  def Download(url: str, path: str = '.'):
+    try:
+      option = {
+        'outtmpl': f'{path}/%(title)s.%(ext)s',
+        'format': 'best',
+      }
+      with YoutubeDL(option) as yt:
+        yt.download([url])
+      print('Download Complete!')
+    except Exception as e:
+      print(f'YT-DL ERROR: {e}')
+   
+  if option.url and option.path:
+    Download(option.url, option.path)
 
-paser = OptionParser()
-
-paser.add_option('-u', '--url', dest='url', help='Enter YouTube URL Video')
-paser.add_option('-p', '--path', dest='path', help='Enter the path where you want to save the video')
-
-(option, args) = paser.parse_args()
-
-def Download(url: str, path: str = '.'):
-  try:
-    option = {
-      'outtmpl': f'{path}/%(title)s.%(ext)s',
-      'format': 'best',
-    }
-    with YoutubeDL(option) as yt:
-      yt.download([url])
-    print('Download Complete!')
-  except Exception as e:
-    print(f'YT-DL ERROR: {e}')
-    
-if option.url and option.path:
-  Download(option.url, option.path)
+except Exception:
+  print('Restart!!')
